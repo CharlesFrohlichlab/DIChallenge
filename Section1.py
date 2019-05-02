374d42796cb2a57390357200d5f991bd62e217ba
THELITTLEMAIDEN\Zhe <zhouz@email.unc.edu>
2019-05-02 17:36:40 -0400

    Added answer to problem 1

--- Section1.py
+++ Section1.py
@@ -16,8 +16,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import geopy as geopy
+import datetime as datetime
 
-data=pd.read_csv("C:\\Users\\Frohlich Lab\\Documents\\GitHub\\NYPD_MotorVehColl\\NYPD_Motor_Vehicle_Collisions.csv")
+data=pd.read_csv("C:\\Users\\Zhe\\Documents\\DataScienceProjects\\NYPD_Collisions\\NYPD_Motor_Vehicle_Collisions.csv")
 
 #Now, our data is loaded. We're writing the following snippet to see the loaded data. The purpose here is to see the top five of the loaded data.
 
@@ -26,12 +28,23 @@ print(data.head(5))
 
 print(data.shape)
 
-# Show summary analytics of data (eg. mean, std, min, max, etc)
-print('Data Show Describe\n')
-print(data.describe())
+print('Are there NaN/Empty entries in the CSV?')
+print( data.isnull().values.any() )
 
-#We will list all the columns for all data. We check all columns. Is there any spelling mistake?
+# Show summary analytics of data (eg. mean, std, min, max, etc)
+#print('Data Show Describe\n')
+#print(data.describe())
+
+#We will list all the columns for all data. We check all columns.
 print('Data Show Columns:\n')
 print(data.columns)
 
 ### We know there are empty values - let's try our best to fill them out
+
+### Problem 1: Number of injuries up until 12/31/2018
+
+# convert date strings to datetime type ; then find entries before the date
+endDate = '12/31/2018'
+mask =  ( pd.to_datetime(data['DATE']) <= endDate ) 
+print(mask.sum())
+
