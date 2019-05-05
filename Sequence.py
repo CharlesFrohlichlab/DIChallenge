0c51b348dd9c6e3a62af5905adedca36643b98dc
THELITTLEMAIDEN\Zhe <zhouz@email.unc.edu>
2019-05-05 11:04:06 -0400

    Sequence: Set up for each problem, finished problem 1

--- Sequence.py
+++ Sequence.py
@@ -2,6 +2,41 @@
 """
 Created on Sat May  4 22:13:29 2019
 
+n: Number of digits in the series
+k: Max value of integers that can be used
+j: Value of the last digit in the series
+
 @author: Zhe
 """
+
+#### Problem 1
+
+n=4
+k=4
+j=2
+
+(k-1)*(k-2)+1
+
+#### Problem 2
+
+n=4 
+k=100 
+j=1
+
+#### Problem 3
+
+n=100 , k=100, and j=1
+
+#### Problem 4
+
+n=347 , k=2281, and j=829
+
+#### Problem 5
+n=1.26⋅106 , k=4.17⋅106  j=1
+
+#### Problem 6
+
+n=107, k=1012, and j=829
+
+
 
